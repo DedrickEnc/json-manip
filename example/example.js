7e61e16f0b5a068e837af9828f9b13eead39c06c
DedrickEnc <kitamuka@gmail.com>
2017-11-05 00:27:16 +0100

    Add delete last key functionnaly

--- example/example.js
+++ example/example.js
@@ -21,3 +21,6 @@ const falseResult = jsonManipulator.isPresent(obj, 'name.food.meal'); // false
 const trueResult1 = jsonManipulator.isPresent(obj, 'references.workMates.2'); //true
 const trueResult2 = jsonManipulator.isPresent(obj, 'name'); //true
 
+const res = jsonManipulator.removeLastKey(obj, 'food.legume');
+
+console.log(res);
